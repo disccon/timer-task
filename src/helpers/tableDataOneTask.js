656deda1edb7fc2f69d584775cebfdddba4d7f2c
Disccon <dissccon@gmail.com>
2019-03-19 12:15:07 +0200

    tableDataOneTask

--- src/helpers/tableDataOneTask.js
+++ src/helpers/tableDataOneTask.js
@@ -1,42 +1,21 @@
 const tableDataOneTask = (timeStart, timeSpendMilliseconds) => {
   const data = []
-  let nowHours2
-  let nowHours
+  let timeSpendMinutes = timeSpendMilliseconds / 1000 / 60
   const timeStartHours = new Date(timeStart).getHours()
   const timeStartMinutes = new Date(timeStart).getMinutes()
-  const timeSpendHours = Math.floor((timeSpendMilliseconds / 3600000) % 60)
-  const timeSpendMinutes = Math.floor((timeSpendMilliseconds / 60000) % 60)
-  let timeSpendHoursLeft
-  let timeSpendMinutesLeft
   for (let i = 0; i < 24; i += 1) {
     let oneHour
+    let minutes = 0
     if (i === timeStartHours) {
-      if (timeSpendHours < 1) {
-        const minutes = 60 - timeStartMinutes
-        if (minutes > timeSpendMinutes) {
-          oneHour = { hour: i, minutes: timeSpendMinutes }
-        } else {
-          oneHour = { hour: i, minutes }
-          timeSpendMinutesLeft = timeSpendMinutes - minutes
-          nowHours2 = i + 1
-        }
-      } else {
-        const minutes = 60 - timeStartMinutes
-        oneHour = { hour: i, minutes }
-        const timeSpend = timeSpendMilliseconds - (minutes * 60000)
-        timeSpendHoursLeft = Math.floor((timeSpend / 3600000) % 60)
-        timeSpendMinutesLeft = Math.floor((timeSpend / 60000) % 60)
-        nowHours = i + 1
-      }
-    } else if (i === nowHours && timeSpendHoursLeft >= 1) {
-      timeSpendHoursLeft -= 1
-      nowHours += 1
-      oneHour = { hour: i, minutes: 60 }
-    } else if (i === nowHours && timeSpendHoursLeft === 0 || i === nowHours2) {
-      oneHour = { hour: i, minutes: timeSpendMinutesLeft }
-    } else {
-      oneHour = { hour: i, minutes: 0 }
+      const minInThisHour = 60 - timeStartMinutes
+      minutes = minInThisHour >= timeSpendMinutes ? timeSpendMinutes : minInThisHour
+      oneHour = { hour: i, minutes: Math.floor(minutes) }
+    } else if (timeSpendMinutes > 0 && i > timeStartHours) {
+      minutes = timeSpendMinutes > 60 ? 60 : timeSpendMinutes
+      oneHour = { hour: i, minutes: Math.floor(minutes) }
     }
+    oneHour = { hour: i, minutes: Math.floor(minutes) }
+    timeSpendMinutes -= minutes
     data.push(oneHour)
   }
   return data
