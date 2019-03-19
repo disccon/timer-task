63465fda670ed19497c0cdf9f5b374f2e356cce0
Disccon <dissccon@gmail.com>
2019-03-19 12:19:46 +0200

    tableDataTasks

--- src/helpers/tableDataTasks.js
+++ src/helpers/tableDataTasks.js
@@ -11,13 +11,8 @@ const tableDataTasks = tasks => {
     data.reduce((previousValue, currentItem, index, arr) => {
       const rowSum = []
       currentItem.forEach((item, i) => {
-        let oneHourSum
         const sumMinutes = previousValue[i].minutes + currentItem[i].minutes
-        if (sumMinutes >= 60) {
-          oneHourSum = { hour: i, minutes: 60 }
-        } else {
-          oneHourSum = { hour: i, minutes: sumMinutes }
-        }
+        const oneHourSum = sumMinutes >= 60 ? { hour: i, minutes: 60 } : { hour: i, minutes: sumMinutes }
         rowSum.push(oneHourSum)
       })
       if ((arr.length - 1) === index) {
