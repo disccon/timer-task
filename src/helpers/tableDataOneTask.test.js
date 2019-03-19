127dca4af3080ab77ec7260a7312e512ab1f4c25
Disccon <dissccon@gmail.com>
2019-03-19 12:45:07 +0200

    test tableDataOneTask

--- src/helpers/tableDataOneTask.test.js
+++ src/helpers/tableDataOneTask.test.js
@@ -18,6 +18,14 @@ it('tableDataOneTask test', () => {
     timeSpend: new Date(2019, 0, 1, 10, 53, 38).getTime() - new Date(2019, 0, 1, 8, 51, 57).getTime(),
   }
 
+  const task3 = {
+    id: 2,
+    taskName: 'long task',
+    timeStart: new Date(2019, 0, 1, 22, 51, 21),
+    timeEnd: new Date(2019, 0, 2, 4, 53, 43),
+    timeSpend: new Date(2019, 0, 2, 4, 53, 43).getTime() - new Date(2019, 0, 1, 22, 51, 21).getTime(),
+  }
+
   const dataChart1 = [{ hour: 0, minutes: 0 }, { hour: 1, minutes: 0 }, { hour: 2, minutes: 0 }, { hour: 3, minutes: 0 },
     { hour: 4, minutes: 0 }, { hour: 5, minutes: 0 }, { hour: 6, minutes: 0 }, { hour: 7, minutes: 32 }, { hour: 8, minutes: 31 },
     { hour: 9, minutes: 0 }, { hour: 10, minutes: 0 }, { hour: 11, minutes: 0 }, { hour: 12, minutes: 0 }, { hour: 13, minutes: 0 },
@@ -30,15 +38,25 @@ it('tableDataOneTask test', () => {
     { hour: 14, minutes: 0 }, { hour: 15, minutes: 0 }, { hour: 16, minutes: 0 }, { hour: 17, minutes: 0 }, { hour: 18, minutes: 0 },
     { hour: 19, minutes: 0 }, { hour: 20, minutes: 0 }, { hour: 21, minutes: 0 }, { hour: 22, minutes: 0 }, { hour: 23, minutes: 0 }]
 
+  const dataChart3 = [{ hour: 0, minutes: 0 }, { hour: 1, minutes: 0 }, { hour: 2, minutes: 0 }, { hour: 3, minutes: 0 },
+    { hour: 4, minutes: 0 }, { hour: 5, minutes: 0 }, { hour: 6, minutes: 0 }, { hour: 7, minutes: 0 }, { hour: 8, minutes: 0 },
+    { hour: 9, minutes: 0 }, { hour: 10, minutes: 0 }, { hour: 11, minutes: 0 }, { hour: 12, minutes: 0 }, { hour: 13, minutes: 0 },
+    { hour: 14, minutes: 0 }, { hour: 15, minutes: 0 }, { hour: 16, minutes: 0 }, { hour: 17, minutes: 0 }, { hour: 18, minutes: 0 },
+    { hour: 19, minutes: 0 }, { hour: 20, minutes: 0 }, { hour: 21, minutes: 0 }, { hour: 22, minutes: 9 }, { hour: 23, minutes: 60 }]
+
   const mockFn = jest.fn(tableDataOneTask)
   mockFn(task1.timeStart, task1.timeSpend)
   mockFn(task2.timeStart, task2.timeSpend)
+  mockFn(task3.timeStart, task3.timeSpend)
   expect(mockFn).toHaveBeenCalled()
-  expect(mockFn.mock.calls.length).toBe(2)
+  expect(mockFn.mock.calls.length).toBe(3)
   expect(mockFn.mock.calls[0][0]).toEqual(task1.timeStart)
   expect(mockFn.mock.calls[0][1]).toEqual(task1.timeSpend)
   expect(mockFn.mock.calls[1][0]).toEqual(task2.timeStart)
   expect(mockFn.mock.calls[1][1]).toEqual(task2.timeSpend)
+  expect(mockFn.mock.calls[2][0]).toEqual(task3.timeStart)
+  expect(mockFn.mock.calls[2][1]).toEqual(task3.timeSpend)
   expect(mockFn.mock.results[0].value).toEqual(dataChart1)
   expect(mockFn.mock.results[1].value).toEqual(dataChart2)
+  expect(mockFn.mock.results[2].value).toEqual(dataChart3)
 })
